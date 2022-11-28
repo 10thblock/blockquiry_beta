082ea823c147ed7f1a9693c962144a6866574313
10thblock <111067610+10thblock@users.noreply.github.com>
2022-11-27 20:58:11 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -91,7 +91,7 @@ export default function Home({ session }) {
     router.push(`/search?term=${term}`);
   };
   const calculateTimeLeft = () => {
-    const difference = +new Date("2022-11-29T11:20:00+09:30") - +new Date();
+    const difference = +new Date("2022-11-29T14:59:00+09:30") - +new Date();
     let timeLeft = {};
 
     if (difference > 0) {
